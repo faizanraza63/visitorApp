6065ae6e23b3a2be57af1d9e05597650e2e0cc1f
Noman9899 <noman.chaudhry989.com>
2025-04-14 10:54:15 +0500

    Added Config file for TestingBot

---
+++ wdio.testingbot.conf.js
@@ -0,0 +1,18 @@
+const base = require('./wdio.local.conf.js');
+
+exports.config = {
+  ...base.config,
+  hostname: 'hub.testingbot.com',
+  port: 443,
+  path: '/wd/hub',
+  user: process.env.TESTINGBOT_KEY,
+  key: process.env.TESTINGBOT_SECRET,
+
+  capabilities: [{
+    platformName: 'Android',
+    deviceName: 'Samsung Galaxy S21',
+    platformVersion: '12',
+    app: 'https://docs.google.com/uc?export=download&id=1_JT_H8U_jWIySRk-x-5bXyo5ltZ8NfNV',  // or your .apk upload URL
+    automationName: 'UiAutomator2',
+  }]
+};
