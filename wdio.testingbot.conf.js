72136d2977a31eecb7d25aa8ea101276956b136a
Noman9899 <noman.chaudhry989.com>
2025-04-14 16:21:47 +0500

    CHanged tb to testingbot in app path in config File

--- wdio.testingbot.conf.js
+++ wdio.testingbot.conf.js
@@ -12,7 +12,7 @@ export const config = {
     platformName: 'Android',
     'appium:deviceName': 'Samsung Galaxy S21',
     'appium:platformVersion': '12',
-    'appium:app': 'tb://c67fb029dfd0022110130f14',  // or your .apk upload URL
+    'appium:app': 'testingbot://c67fb029dfd0022110130f14',  // or your .apk upload URL
     'appium:automationName': 'UiAutomator2',
   }]
 };
